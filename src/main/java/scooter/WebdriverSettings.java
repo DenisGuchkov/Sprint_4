985118e3ea727beb40b11282fafd514813142000
Denis Guchkov <denistomskk@yandex.ru>
2023-03-09 02:56:22 +0700

    test third

--- src/main/java/scooter/WebdriverSettings.java
+++ src/main/java/scooter/WebdriverSettings.java
@@ -1,4 +1,4 @@
-package ScooterPracticumServices;
+package scooter;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
--- src/test/java/scooter/OrderScooterTest.java
+++ src/test/java/scooter/OrderScooterTest.java
@@ -1,6 +1,5 @@
 package scooter;
 
-import ScooterPracticumServices.WebdriverSettings;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
